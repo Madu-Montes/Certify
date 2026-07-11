a7b1dd8d9cdf29291511ab2306a515f9545b81b1
madu-montes <monteseduarda56@gmail.com>
2026-07-10 22:59:53 -0300

    feat: header and hover, with style config

--- certify/src/app/app.ts
+++ certify/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Header,
+    Hero,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
