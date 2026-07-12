6b758c10e373b37f9b0f98b2b870b71510da1cb6
madu-montes <monteseduarda56@gmail.com>
2026-07-12 19:52:10 -0300

    feat: finished the structure of the main page

--- certify/src/app/app.ts
+++ certify/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { Hero } from './components/hero/hero';
     RouterOutlet,
     Header,
     Hero,
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
