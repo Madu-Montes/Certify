b2880a90e7db76c2f34628a33e36f243c29132a6
madu-montes <monteseduarda56@gmail.com>
2026-07-13 21:58:52 -0300

    feat: add the spreadsheet

--- certify/src/app/app.routes.ts
+++ certify/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { UploadCard } from './components/upload-card/upload-card';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'upload',
+    component: UploadCard
+  }
+];
--- certify/src/app/components/hero/hero.ts
+++ certify/src/app/components/hero/hero.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+    private readonly router = inject(Router);
+
+  goToUpload() {
+    this.router.navigate(['/upload']);
+    console.log('teste')
+  }
+
+  downloadSpreadsheet(): void {
+    const link = document.createElement('a');
+    link.href = '/templates/planilha-modelo.xlsx';
+    link.download = 'planilha-modelo.xlsx';
+    link.click();
+  }
+}
