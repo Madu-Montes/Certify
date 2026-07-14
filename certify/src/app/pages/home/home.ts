de234824e7236affb02384f79766702305eed897
madu-montes <monteseduarda56@gmail.com>
2026-07-14 09:36:34 -0300

    fix: conetion hero to upload is fixed

--- certify/src/app/pages/home/home.ts
+++ certify/src/app/pages/home/home.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  private readonly router = inject(Router);
+
+  goToUpload() {
+    this.router.navigate(['/upload']);
+    console.log('teste');
+  }
+
+  downloadSpreadsheet(): void {
+    const link = document.createElement('a');
+    link.href = '/templates/planilha-modelo.xlsx';
+    link.download = 'planilha-modelo.xlsx';
+    link.click();
+  }
+}
