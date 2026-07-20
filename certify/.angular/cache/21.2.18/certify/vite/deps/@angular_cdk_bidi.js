afe4f507fbb992ba6b4a52b71c2a3045e454c93e
madu-montes <monteseduarda56@gmail.com>
2026-07-20 20:49:25 -0300

    feat: add initial backend setup

---
+++ certify/.angular/cache/21.2.18/certify/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,13 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-QIZCYINO.js";
+import "./chunk-25PXA3AX.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
